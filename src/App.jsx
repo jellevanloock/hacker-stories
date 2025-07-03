86297a835192f43f585032c163087e9103de71a7
Jelle Van Loock <22350389+jellevanloock@users.noreply.github.com>
2025-07-03 19:17:57 +0200

    Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-function getTitle(title) {
-    return title;
-}
+const list = [
+    {
+        title: 'React',
+        url: 'https://react.dev/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 
 function App() {
     return (
         <div>
-            <h1>Hello {getTitle('React')}</h1>
+            <h1>My Hacker Stories</h1>
 
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
+
+            <hr />
+
+            <ul>
+                {list.map((el, i) => {
+                    {
+                        /* only use an index as last resort */
+                    }
+                    return (
+                        <li key={el.objectID}>
+                            <p></p>
+                            <span>
+                                <a href={el.url}>{el.title}</a>
+                            </span>
+                            <span>{el.author}</span>
+                            <span>{el.num_comments}</span>
+                            <span>{el.points}</span>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
